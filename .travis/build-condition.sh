b5fad3940681f4bdeabc7d4c591820c81640581b
niktrix <nicky.gurbani@gmail.com>
2018-10-08 16:40:00 +0530

    install jest globally

--- .travis/build-condition.sh
+++ .travis/build-condition.sh
@@ -5,3 +5,4 @@ wget https://github.com/enKryptIO/ethvm/blob/master/provisioners/docker/config/m
 mongo --eval "rs.initiate()"
 mongo < ./bin/mongo/init.js
 mongorestore -h 127.0.0.1 --port 27017 --db ethvm_local --archive="ethvm_sample.archive?raw=true"
+yarn global add jest
