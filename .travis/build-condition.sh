a80b881687f12fc70864b9688107d81c32ff8e3f
niktrix <nicky.gurbani@gmail.com>
2018-10-08 16:00:11 +0530

    update travis

--- .travis/build-condition.sh
+++ .travis/build-condition.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-if [[ -z $1 ]]; then
-    echo "Commit range cannot be empty"
-    exit 1
-fi
-
-if [[ -z $2 ]]; then
-    echo "Change path cannot be empty"
-    exit 1
-fi
-
-git diff --name-only $1 | sort -u | uniq | grep $2 > /dev/null
+echo "deb http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod start
+wget https://github.com/enKryptIO/ethvm/blob/master/provisioners/docker/config/mongo/ethvm_sample.archive?raw=true
+mongorestore -h 127.0.0.1 --port 27017 ethvm_sample.archive
