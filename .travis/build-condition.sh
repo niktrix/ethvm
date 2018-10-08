42d72880bdb05308039796d0ede4536b098a065a
niktrix <nicky.gurbani@gmail.com>
2018-10-08 16:17:30 +0530

    sudo not allowd in travis

--- .travis/build-condition.sh
+++ .travis/build-condition.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "deb http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo service mongod start
+apt-get update
+apt-get install -y mongodb-org
+service mongod start
+mongod --version
 wget https://github.com/enKryptIO/ethvm/blob/master/provisioners/docker/config/mongo/ethvm_sample.archive?raw=true
 mongorestore -h 127.0.0.1 --port 27017 ethvm_sample.archive
