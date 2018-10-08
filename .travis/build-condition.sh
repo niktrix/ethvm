a1ac45415841495492c156f8fc245d5cb3273a8c
niktrix <nicky.gurbani@gmail.com>
2018-10-08 16:30:49 +0530

    rename archieve file

--- .travis/build-condition.sh
+++ .travis/build-condition.sh
@@ -2,4 +2,4 @@
 
 
 wget https://github.com/enKryptIO/ethvm/blob/master/provisioners/docker/config/mongo/ethvm_sample.archive?raw=true
-mongorestore -h 127.0.0.1 --port 27017 ethvm_sample.archive
+mongorestore -h 127.0.0.1 --port 27017 ethvm_sample.archive?raw=true
