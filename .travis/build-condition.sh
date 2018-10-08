b17341f288f31e14feb0c915902610f2a3a88f60
niktrix <nicky.gurbani@gmail.com>
2018-10-08 16:36:42 +0530

    init mongo

--- .travis/build-condition.sh
+++ .travis/build-condition.sh
@@ -2,4 +2,6 @@
 
 
 wget https://github.com/enKryptIO/ethvm/blob/master/provisioners/docker/config/mongo/ethvm_sample.archive?raw=true
-mongorestore -h 127.0.0.1 --port 27017 ethvm_sample.archive?raw=true
+mongo --eval "rs.initiate()"
+mongo < ./bin/mongo/init.js
+mongorestore -h 127.0.0.1 --port 27017 --db ethvm_local --archive="ethvm_sample.archive?raw=true"
