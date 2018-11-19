6bb28f7e848b8eaaaead8e83526f825663d35b54
niktrix <nicky.gurbani@gmail.com>
2018-11-19 20:51:51 +0530

    change dir to root

--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -6,5 +6,6 @@ ROOT_DIR=$(cd ${SCRIPT_DIR}/..; pwd)
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
+cd $ROOT_DIR
 $ROOT_DIR/bin/docker-build.sh build api
 $ROOT_DIR/bin/docker-build.sh push api
