1b5feb13f88c0f4c8fa0072a2e85a9de153c738c
niktrix <nicky.gurbani@gmail.com>
2018-11-19 21:06:38 +0530

    deploy explorer

--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -9,3 +9,6 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 cd $ROOT_DIR
 $ROOT_DIR/bin/docker-build.sh build api
 $ROOT_DIR/bin/docker-build.sh push api
+
+$ROOT_DIR/bin/docker-build.sh build explorer
+$ROOT_DIR/bin/docker-build.sh push explorer
