4cd8a5be2ab653df1e79f8e05c1a07df4562c610
niktrix <nicky.gurbani@gmail.com>
2018-11-19 20:47:11 +0530

    update for jq

--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -3,7 +3,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR=$(cd ${SCRIPT_DIR}/..; pwd)
 
-sudo apt-get install jq
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
